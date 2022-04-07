f6d6f0fc19f2966ed5e1f58a055a8f011f9d8d99
Andressa Buglio <103058808+andressabuglio@users.noreply.github.com>
2022-04-07 01:30:59 -0300

    Add files via upload

---
+++ dotnet - poo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using dotnet___poo.src.Entities;
+
+namespace dotnet___poo 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Knight arus = new Knight("Arus", 23, "Knight");
+            Wizard wizard = new Wizard("Jennica", 23, "White Wizard");
+
+
+            Console.WriteLine(wizard.Attack(1));
+            Console.WriteLine(wizard.Attack(7));
+        }
+    }
+}
---
+++ dotnet - poo/src/Entities/Wizard.cs
@@ -0,0 +1,25 @@
+namespace dotnet___poo.src.Entities
+{
+    public class Wizard : Hero  
+    {
+        public Wizard(string Name, int level, string HeroType)
+        {
+            this.Name = Name;
+            this.level = level;
+            this.HeroType = HeroType;
+        }
+        public override string Attack(){
+            return this.Name + " Lançou Magia";
+    }
+
+    public string Attack(int Bonus){
+
+        if (Bonus > 6){
+            return this.Name + " Lançou Magia super efetiva com bonus de " + Bonus;
+        }else{
+            return this.Name + " Lançou uma Magia com força fraca com bonus de " + Bonus;
+        }
+
+        }
+    }
+}
